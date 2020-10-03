3ced227e8a3ba87b59419b74a5f5872e19545f0f
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-10-03 16:29:48 +0200

    Test the exit commands

--- tools/travis.sh
+++ tools/travis.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-./flutter/bin/flutter packages get || EXIT_CODE=$?
-./flutter/bin/flutter pub global activate dart_style || EXIT_CODE=$?
+./flutter/bin/flutter packages get || { echo 'my_command failed' ; exit -1; }
+./flutter/bin/flutter pub global activate dart_style || { echo 'my_command failed' ; exit -1; }
 
 #echo "ANALYZE"
-#./flutter/bin/flutter analyze || EXIT_CODE=$?
+#./flutter/bin/flutter analyze || { echo 'my_command failed' ; exit -1; }
 
 echo "DART FORMAT CHECK"
-.pub-cache/bin/dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
+./.pub-cache/bin/dartfmt -n --set-exit-if-changed .  || { echo 'my_command failed' ; exit -1; }
 
 echo "TEST"
-./flutter/bin/flutter test || EXIT_CODE=$?
+./flutter/bin/flutter test || { echo 'my_command failed' ; exit -1; }
