55eb3388e36f2fc03a9f3bd0491f0266bfb8b8da
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-10-03 17:11:29 +0200

    Enable the analyzer

--- tools/travis.sh
+++ tools/travis.sh
@@ -5,8 +5,8 @@ ls -la
 flutter packages get || exit -1;
 flutter pub global activate dart_style || exit -1;
 
-#echo "ANALYZE"
-#flutter analyze || exit -1;
+echo "ANALYZE"
+flutter analyze || exit -1;
 
 echo "DART FORMAT CHECK"
 flutter pub global run dart_style:format -n --set-exit-if-changed . || exit -1;
