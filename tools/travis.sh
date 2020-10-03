bb26f9f3c140d6d4c7ff82f2e4a91ee792d16887
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-10-03 17:08:41 +0200

    Moved the flutter installation to a lower folder

--- tools/travis.sh
+++ tools/travis.sh
@@ -2,14 +2,14 @@
 
 ls -la
 
-./flutter/bin/flutter packages get || exit -1;
-./flutter/bin/flutter pub global activate dart_style || exit -1;
+flutter packages get || exit -1;
+flutter pub global activate dart_style || exit -1;
 
 #echo "ANALYZE"
 #flutter analyze || exit -1;
 
 echo "DART FORMAT CHECK"
-./flutter/bin/flutter pub global run dart_style:format -n --set-exit-if-changed . || exit -1;
+flutter pub global run dart_style:format -n --set-exit-if-changed . || exit -1;
 
 echo "TEST"
-./flutter/bin/flutter test || exit -1;
+flutter test || exit -1;
