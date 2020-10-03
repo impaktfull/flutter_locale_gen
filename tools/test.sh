a9cae64bc858e6f1b55c738b01d6b4beb007239e
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-10-04 01:33:28 +0200

    Updated test script

--- tools/test.sh
+++ tools/test.sh
@@ -2,20 +2,8 @@
 
 flutter test --coverage || exit -1;
 
-echo "LS BASE"
-ls
-
-echo "CD .."
-cd ..
-
-echo "LS ROOT PROJECT"
-
-ls
-echo "CD COVERAGE"
-
 cd coverage
 
-echo "LS COVERAGE"
 ls
 
-coveralls-lcov coverage/lcov.info
+coveralls-lcov lcov.info
