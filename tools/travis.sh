ca9f94633d1281467a0730677da96d6e01ee2d90
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-10-03 16:36:25 +0200

    Changed PATH

--- tools/travis.sh
+++ tools/travis.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-./flutter/bin/flutter packages get || { echo 'my_command failed' ; exit -1; }
-./flutter/bin/flutter pub global activate dart_style || { echo 'my_command failed' ; exit -1; }
+flutter packages get || exit -1;
+flutter pub global activate dart_style || exit -1;
 
 #echo "ANALYZE"
-#./flutter/bin/flutter analyze || { echo 'my_command failed' ; exit -1; }
+#flutter analyze || exit -1;
 
 echo "DART FORMAT CHECK"
-./.pub-cache/bin/dartfmt -n --set-exit-if-changed .  || { echo 'my_command failed' ; exit -1; }
+dartfmt -n --set-exit-if-changed . || exit -1;
 
 echo "TEST"
-./flutter/bin/flutter test || { echo 'my_command failed' ; exit -1; }
+flutter test || exit -1;
