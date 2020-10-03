e3ed08e6ccaa71e57dcbf8bbf78e45710abc69e0
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2020-10-03 16:41:51 +0200

    Changed flutter install

--- tools/travis.sh
+++ tools/travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-flutter packages get || exit -1;
-flutter pub global activate dart_style || exit -1;
+./flutter/bin/flutter packages get || exit -1;
+./flutter/bin/flutter pub global activate dart_style || exit -1;
 
 #echo "ANALYZE"
 #flutter analyze || exit -1;
@@ -10,4 +10,4 @@ echo "DART FORMAT CHECK"
 dartfmt -n --set-exit-if-changed . || exit -1;
 
 echo "TEST"
-flutter test || exit -1;
+./flutter/bin/flutter test || exit -1;
