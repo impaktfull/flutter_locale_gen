9b8ea205b4f6c5bc6a97479aa863e3ba961920c8
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2021-03-30 09:35:16 +0200

    Fixed formatting

--- test/translation_writer_test.dart
+++ test/translation_writer_test.dart
@@ -7,88 +7,146 @@ void main() {
       test('Test null translations', () {
         final sb = StringBuffer();
         TranslationWriter.buildTranslationFunction(sb, 'app_title', null);
-        expect(sb.toString(), equals('  String get appTitle => _t(LocalizationKeys.appTitle);\n\n'));
+        expect(
+            sb.toString(),
+            equals(
+                '  String get appTitle => _t(LocalizationKeys.appTitle);\n\n'));
       });
       test('Test empty translations', () {
         final sb = StringBuffer();
         TranslationWriter.buildTranslationFunction(sb, 'app_title', '');
-        expect(sb.toString(), equals('  String get appTitle => _t(LocalizationKeys.appTitle);\n\n'));
+        expect(
+            sb.toString(),
+            equals(
+                '  String get appTitle => _t(LocalizationKeys.appTitle);\n\n'));
       });
       test('Test translations without arguments', () {
         final sb = StringBuffer();
         TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo');
-        expect(sb.toString(), equals('  String get appTitle => _t(LocalizationKeys.appTitle);\n\n'));
+        expect(
+            sb.toString(),
+            equals(
+                '  String get appTitle => _t(LocalizationKeys.appTitle);\n\n'));
       });
     });
 
     group('Tests with string arguments', () {
       test('Test translations with 1 string argument', () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$s');
-        expect(sb.toString(), equals('  String appTitle(String arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
+        TranslationWriter.buildTranslationFunction(
+            sb, 'app_title', 'hallo %1\$s');
+        expect(
+            sb.toString(),
+            equals(
+                '  String appTitle(String arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
       });
 
       test('Test translations with 2 string arguments', () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$s %2\$s');
-        expect(sb.toString(), equals('  String appTitle(String arg1, String arg2) => _t(LocalizationKeys.appTitle, args: [arg1, arg2]);\n\n'));
+        TranslationWriter.buildTranslationFunction(
+            sb, 'app_title', 'hallo %1\$s %2\$s');
+        expect(
+            sb.toString(),
+            equals(
+                '  String appTitle(String arg1, String arg2) => _t(LocalizationKeys.appTitle, args: [arg1, arg2]);\n\n'));
       });
 
-      test('Test translations with 1 string argument but 2 string replacements', () {
+      test('Test translations with 1 string argument but 2 string replacements',
+          () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$s %1\$s');
-        expect(sb.toString(), equals('  String appTitle(String arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
+        TranslationWriter.buildTranslationFunction(
+            sb, 'app_title', 'hallo %1\$s %1\$s');
+        expect(
+            sb.toString(),
+            equals(
+                '  String appTitle(String arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
       });
 
-      test('Test translations with 11 arguments of the same index and same type', () {
+      test(
+          'Test translations with 11 arguments of the same index and same type',
+          () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s');
-        expect(sb.toString(), equals('  String appTitle(String arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
+        TranslationWriter.buildTranslationFunction(sb, 'app_title',
+            'hallo %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s');
+        expect(
+            sb.toString(),
+            equals(
+                '  String appTitle(String arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
       });
     });
 
     group('Tests with number arguments', () {
       test('Test translations with 1 number argument', () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$d');
-        expect(sb.toString(), equals('  String appTitle(num arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
+        TranslationWriter.buildTranslationFunction(
+            sb, 'app_title', 'hallo %1\$d');
+        expect(
+            sb.toString(),
+            equals(
+                '  String appTitle(num arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
       });
 
       test('Test translations with 2 number arguments', () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$d %2\$d');
-        expect(sb.toString(), equals('  String appTitle(num arg1, num arg2) => _t(LocalizationKeys.appTitle, args: [arg1, arg2]);\n\n'));
+        TranslationWriter.buildTranslationFunction(
+            sb, 'app_title', 'hallo %1\$d %2\$d');
+        expect(
+            sb.toString(),
+            equals(
+                '  String appTitle(num arg1, num arg2) => _t(LocalizationKeys.appTitle, args: [arg1, arg2]);\n\n'));
       });
 
-      test('Test translations with 1 number argument but 2 number replacements', () {
+      test('Test translations with 1 number argument but 2 number replacements',
+          () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$d %1\$d');
-        expect(sb.toString(), equals('  String appTitle(num arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
+        TranslationWriter.buildTranslationFunction(
+            sb, 'app_title', 'hallo %1\$d %1\$d');
+        expect(
+            sb.toString(),
+            equals(
+                '  String appTitle(num arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
       });
 
-      test('Test translations with 11 arguments of the same index and same type', () {
+      test(
+          'Test translations with 11 arguments of the same index and same type',
+          () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d');
-        expect(sb.toString(), equals('  String appTitle(num arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
+        TranslationWriter.buildTranslationFunction(sb, 'app_title',
+            'hallo %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d %1\$d');
+        expect(
+            sb.toString(),
+            equals(
+                '  String appTitle(num arg1) => _t(LocalizationKeys.appTitle, args: [arg1]);\n\n'));
       });
     });
 
     group('Tests with mixed arguments', () {
       test('Test translations with 1 number argument', () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$s %2\$d');
-        expect(sb.toString(), equals('  String appTitle(String arg1, num arg2) => _t(LocalizationKeys.appTitle, args: [arg1, arg2]);\n\n'));
+        TranslationWriter.buildTranslationFunction(
+            sb, 'app_title', 'hallo %1\$s %2\$d');
+        expect(
+            sb.toString(),
+            equals(
+                '  String appTitle(String arg1, num arg2) => _t(LocalizationKeys.appTitle, args: [arg1, arg2]);\n\n'));
       });
 
-      test('Test translations with 11 arguments of the same index and same type', () {
+      test(
+          'Test translations with 11 arguments of the same index and same type',
+          () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %2\$d');
-        expect(sb.toString(), equals('  String appTitle(String arg1, num arg2) => _t(LocalizationKeys.appTitle, args: [arg1, arg2]);\n\n'));
+        TranslationWriter.buildTranslationFunction(sb, 'app_title',
+            'hallo %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %1\$s %2\$d');
+        expect(
+            sb.toString(),
+            equals(
+                '  String appTitle(String arg1, num arg2) => _t(LocalizationKeys.appTitle, args: [arg1, arg2]);\n\n'));
       });
 
       test('Test translations with 11 different arguments', () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$s %2\$s %3\$s %4\$s %5\$s %6\$s %7\$s %8\$s %9\$s %10\$s %11\$s');
+        TranslationWriter.buildTranslationFunction(sb, 'app_title',
+            'hallo %1\$s %2\$s %3\$s %4\$s %5\$s %6\$s %7\$s %8\$s %9\$s %10\$s %11\$s');
         expect(
             sb.toString(),
             equals(
@@ -99,14 +157,22 @@ void main() {
     group('Tests with invalid arguments', () {
       test('Test translations with unsupported type', () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$z');
-        expect(sb.toString(), equals('  String get appTitle => _t(LocalizationKeys.appTitle);\n\n'));
+        TranslationWriter.buildTranslationFunction(
+            sb, 'app_title', 'hallo %1\$z');
+        expect(
+            sb.toString(),
+            equals(
+                '  String get appTitle => _t(LocalizationKeys.appTitle);\n\n'));
       });
 
       test('Test translations with 1 number argument', () {
         final sb = StringBuffer();
-        TranslationWriter.buildTranslationFunction(sb, 'app_title', 'hallo %1\$s %1\$d');
-        expect(sb.toString(), equals('  String get appTitle => _t(LocalizationKeys.appTitle);\n\n'));
+        TranslationWriter.buildTranslationFunction(
+            sb, 'app_title', 'hallo %1\$s %1\$d');
+        expect(
+            sb.toString(),
+            equals(
+                '  String get appTitle => _t(LocalizationKeys.appTitle);\n\n'));
       });
     });
   });
