c7d1ac2572a2cb920cf33c132df76d3c8fa985cd
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2025-08-20 16:12:28 +0200

    feat: only export what is required

--- test/locale_gen_test.dart
+++ test/locale_gen_test.dart
@@ -5,7 +5,6 @@ import 'package:test/test.dart';
 void main() {
   final allowedExports = [
     'src/model/locale_gen_params.dart',
-    'src/model/locale_gen_type.dart',
     'src/locale_gen_writer.dart',
     'src/util/format/locale_gen_formatter.dart',
   ];
@@ -13,17 +12,13 @@ void main() {
     final file = File('lib/locale_gen.dart');
     expect(file.existsSync(), true);
     final content = file.readAsLinesSync();
-    final fullAllowedExports =
-        allowedExports.map((e) => 'export \'$e\';').toList();
+    final fullAllowedExports = allowedExports.map((e) => 'export \'$e\';').toList();
     var amountOfExports = 0;
     for (final line in content) {
-      expect(line.contains('package:locale_gen/'), false,
-          reason: '$line should not contain package:locale_gen/');
-      expect(fullAllowedExports.contains(line), true,
-          reason: '$line is not allowed');
+      expect(line.contains('package:locale_gen/'), false, reason: '$line should not contain package:locale_gen/');
+      expect(fullAllowedExports.contains(line), true, reason: '$line is not allowed');
       amountOfExports++;
     }
-    expect(amountOfExports, allowedExports.length,
-        reason: 'Amount of exports should be ${allowedExports.length}');
+    expect(amountOfExports, allowedExports.length, reason: 'Amount of exports should be ${allowedExports.length}');
   });
 }
