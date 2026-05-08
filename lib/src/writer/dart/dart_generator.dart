dda06a3187c279f31c376adb9ddd104e08ec23c4
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2026-05-08 23:11:28 +0200

    feat: MessageFormat support in Dart (non-Flutter) writer

--- lib/src/writer/dart/dart_generator.dart
+++ lib/src/writer/dart/dart_generator.dart
@@ -1,8 +1,74 @@
 import 'package:locale_gen/src/model/locale_gen_params.dart';
+import 'package:locale_gen/src/model/message_format_ast.dart';
+import 'package:locale_gen/src/model/message_format_param.dart';
 import 'package:locale_gen/src/model/plural.dart';
 import 'package:locale_gen/src/util/case/case_util.dart';
 import 'package:locale_gen/src/util/documentation/documentation_util.dart';
+import 'package:locale_gen/src/util/parser/message_format_parser.dart';
+import 'package:locale_gen/src/util/parser/translation_style_detector.dart';
+import 'package:locale_gen/src/util/validation/cross_locale_validator.dart';
 import 'package:locale_gen/src/writer/core_generator.dart';
+
+const _dartDurationHelperTemplate = r'''
+  String _formatDuration(Duration d, String? style) {
+    final h = d.inHours;
+    final m = d.inMinutes.remainder(60);
+    final s = d.inSeconds.remainder(60);
+    String pad(int n) => n.toString().padLeft(2, '0');
+    if (style == null || style == 'medium') {
+      return '${pad(h)}:${pad(m)}:${pad(s)}';
+    }
+    if (style == 'short') {
+      return '${pad(d.inMinutes)}:${pad(s)}';
+    }
+    if (style == 'long') {
+      final parts = <String>[];
+      if (h > 0) parts.add('${h}h');
+      if (m > 0) parts.add('${m}m');
+      if (s > 0 || parts.isEmpty) parts.add('${s}s');
+      return parts.join(' ');
+    }
+    final buf = StringBuffer();
+    var i = 0;
+    while (i < style.length) {
+      final c = style[i];
+      if (c == "'" && i + 1 < style.length) {
+        final end = style.indexOf("'", i + 1);
+        if (end == -1) {
+          buf.write(style.substring(i + 1));
+          break;
+        }
+        buf.write(style.substring(i + 1, end));
+        i = end + 1;
+        continue;
+      }
+      if (c == 'H') {
+        var n = 1;
+        while (i + n < style.length && style[i + n] == 'H') n++;
+        buf.write(h.toString().padLeft(n, '0'));
+        i += n;
+        continue;
+      }
+      if (c == 'm') {
+        var n = 1;
+        while (i + n < style.length && style[i + n] == 'm') n++;
+        buf.write(m.toString().padLeft(n, '0'));
+        i += n;
+        continue;
+      }
+      if (c == 's') {
+        var n = 1;
+        while (i + n < style.length && style[i + n] == 's') n++;
+        buf.write(s.toString().padLeft(n, '0'));
+        i += n;
+        continue;
+      }
+      buf.write(c);
+      i++;
+    }
+    return buf.toString();
+  }
+''';
 
 class LocaleGenDartGenerator extends LocaleGenCoreGenerator {
   String createLocalizationFile(
@@ -14,8 +80,53 @@ class LocaleGenDartGenerator extends LocaleGenCoreGenerator {
     if (hasPlurals) {
       throw Exception('Plurals are not supported for `dart` writer');
     }
+
+    final hasMessageFormat = defaultTranslations.values.whereType<String>().any(
+        (v) =>
+            TranslationStyleDetector.detect(v) ==
+            TranslationStyle.messageFormat);
+
+    final hasDuration = defaultTranslations.values.whereType<String>().any((v) {
+      if (TranslationStyleDetector.detect(v) != TranslationStyle.messageFormat) {
+        return false;
+      }
+      try {
+        final ast = MessageFormatParser.parse(v);
+        return _astContainsDuration(ast);
+      } on MessageFormatParseException {
+        return false;
+      }
+    });
+
+    if (hasMessageFormat) {
+      defaultTranslations.forEach((key, dynamic value) {
+        if (value is! String) return;
+        if (TranslationStyleDetector.detect(value) !=
+            TranslationStyle.messageFormat) {
+          return;
+        }
+        final others = <String, String>{};
+        for (final entry in allTranslations.entries) {
+          if (entry.key == params.defaultLanguage) continue;
+          final v = entry.value[key];
+          if (v is String) others[entry.key] = v;
+        }
+        final warnings = CrossLocaleValidator.validateKey(
+          key: key,
+          defaultLanguage: params.defaultLanguage,
+          defaultValue: value,
+          otherLocales: others,
+        );
+        for (final w in warnings) {
+          print(w);
+        }
+      });
+    }
+
     final sb = StringBuffer();
     [
+      if (hasMessageFormat) ...["import 'package:intl/intl.dart';"],
+      if (hasMessageFormat) ...["import 'package:intl/message_format.dart';"],
       "import 'package:sprintf/sprintf.dart';",
     ]
       ..sort((i1, i2) => i1.compareTo(i2))
@@ -61,6 +172,30 @@ class LocaleGenDartGenerator extends LocaleGenCoreGenerator {
       ..writeln('    }')
       ..writeln('  }')
       ..writeln();
+    if (hasMessageFormat) {
+      sb
+        ..writeln(
+            '  String _mf(String template, {required Map<String, Object> args, required String locale}) {')
+        ..writeln('    try {')
+        ..writeln('      final stripped = _stripFormatSpecs(template);')
+        ..writeln(
+            '      return MessageFormat(stripped, locale: locale).format(args);')
+        ..writeln('    } catch (e) {')
+        ..writeln("      return '⚠\$template⚠';")
+        ..writeln('    }')
+        ..writeln('  }')
+        ..writeln()
+        ..writeln('  String _stripFormatSpecs(String value) {')
+        ..writeln(
+            "    final regex = RegExp(r'\\{(\\w+)\\s*,\\s*(number|date|time|duration)(\\s*,[^{}]*)?\\}');")
+        ..writeln(
+            "    return value.replaceAllMapped(regex, (m) => '{\${m.group(1)}}');")
+        ..writeln('  }')
+        ..writeln();
+    }
+    if (hasDuration) {
+      sb..write(_dartDurationHelperTemplate)..writeln();
+    }
     defaultTranslations.forEach((key, dynamic value) {
       DocumentationUtil.buildDocumentation(
           sb, key, allTranslations, params.docLanguages);
@@ -180,5 +315,139 @@ class LocaleGenDartGenerator extends LocaleGenCoreGenerator {
     Map<String, Map<String, dynamic>> allTranslations,
   ) {
     throw ArgumentError('Plurals are not supported for `dart` writer');
+  }
+
+  @override
+  void buildMessageFormatFunction(
+    StringBuffer sb,
+    LocaleGenParams params,
+    String key,
+    MessageFormatAst ast,
+    Map<String, MessageFormatParam> mfParams,
+    Map<String, Map<String, dynamic>> allTranslations,
+  ) {
+    final camelKey = CaseUtil.getCamelcase(key);
+    final paramSignatures = mfParams.values
+        .map((p) => 'required ${_dartTypeFor(p.dartType)} ${p.dartName}')
+        .join(', ');
+    if (mfParams.isEmpty) {
+      sb.writeln('  LocalizedValue get $camelKey => LocalizedValue(');
+    } else {
+      sb.writeln(
+          '  LocalizedValue $camelKey({$paramSignatures}) => LocalizedValue(');
+    }
+    for (final locale in params.languages) {
+      final variableName = locale.replaceAll('-', '');
+      final localeTranslations = allTranslations[locale];
+      if (localeTranslations == null) {
+        throw Exception(
+            'Locale $locale not found in allTranslations for key $key');
+      }
+      final template = localeTranslations[key];
+      if (template is! String) {
+        throw Exception(
+            'Key $key in locale $locale is not a String for MessageFormat');
+      }
+      final escapedTemplate = _getEscapedValue(template);
+      if (mfParams.isEmpty) {
+        sb.writeln(
+            '    $variableName: _mf(r"$escapedTemplate", args: const {}, locale: \'$locale\'),');
+      } else {
+        final argEntries = mfParams.values
+            .map((p) =>
+                "'${p.originalName}': ${_argExpressionForLocale(p, locale)}")
+            .join(', ');
+        sb.writeln(
+            '    $variableName: _mf(r"$escapedTemplate", args: {$argEntries}, locale: \'$locale\'),');
+      }
+    }
+    sb
+      ..writeln('  );')
+      ..writeln();
+  }
+
+  static String _dartTypeFor(MessageFormatParamType type) {
+    switch (type) {
+      case MessageFormatParamType.string:
+        return 'String';
+      case MessageFormatParamType.num_:
+        return 'num';
+      case MessageFormatParamType.dateTime:
+        return 'DateTime';
+      case MessageFormatParamType.duration:
+        return 'Duration';
+    }
+  }
+
+  static String _argExpressionForLocale(
+      MessageFormatParam p, String localeTag) {
+    final tag = "'$localeTag'";
+    switch (p.formatter) {
+      case MessageFormatFormatter.none:
+        return p.dartName;
+      case MessageFormatFormatter.numberDecimal:
+        return 'NumberFormat.decimalPattern($tag).format(${p.dartName})';
+      case MessageFormatFormatter.numberPercent:
+        return 'NumberFormat.percentPattern($tag).format(${p.dartName})';
+      case MessageFormatFormatter.numberCurrency:
+        return 'NumberFormat.simpleCurrency(locale: $tag).format(${p.dartName})';
+      case MessageFormatFormatter.numberCustom:
+        return "NumberFormat('${_escape(p.formatterStyle ?? '')}', $tag).format(${p.dartName})";
+      case MessageFormatFormatter.dateShort:
+        return 'DateFormat.yMd($tag).format(${p.dartName})';
+      case MessageFormatFormatter.dateMedium:
+        return 'DateFormat.yMMMd($tag).format(${p.dartName})';
+      case MessageFormatFormatter.dateLong:
+        return 'DateFormat.yMMMMd($tag).format(${p.dartName})';
+      case MessageFormatFormatter.dateFull:
+        return 'DateFormat.yMMMMEEEEd($tag).format(${p.dartName})';
+      case MessageFormatFormatter.dateCustom:
+        return "DateFormat('${_escape(p.formatterStyle ?? '')}', $tag).format(${p.dartName})";
+      case MessageFormatFormatter.timeShort:
+        return 'DateFormat.jm($tag).format(${p.dartName})';
+      case MessageFormatFormatter.timeMedium:
+      case MessageFormatFormatter.timeLong:
+      case MessageFormatFormatter.timeFull:
+        return 'DateFormat.jms($tag).format(${p.dartName})';
+      case MessageFormatFormatter.timeCustom:
+        return "DateFormat('${_escape(p.formatterStyle ?? '')}', $tag).format(${p.dartName})";
+      case MessageFormatFormatter.durationDefault:
+      case MessageFormatFormatter.durationMedium:
+        return '_formatDuration(${p.dartName}, null)';
+      case MessageFormatFormatter.durationShort:
+        return "_formatDuration(${p.dartName}, 'short')";
+      case MessageFormatFormatter.durationLong:
+        return "_formatDuration(${p.dartName}, 'long')";
+      case MessageFormatFormatter.durationCustom:
+        return "_formatDuration(${p.dartName}, '${_escape(p.formatterStyle ?? '')}')";
+    }
+  }
+
+  static String _escape(String s) =>
+      s.replaceAll(r'\', r'\\').replaceAll("'", r"\'");
+
+  static bool _astContainsDuration(MessageFormatAst ast) {
+    bool walk(MessageFormatNode node) {
+      switch (node) {
+        case DurationNode():
+          return true;
+        case PluralNode(:final branches):
+        case SelectOrdinalNode(:final branches):
+        case SelectNode(:final branches):
+          for (final list in branches.values) {
+            for (final n in list) {
+              if (walk(n)) return true;
+            }
+          }
+          return false;
+        default:
+          return false;
+      }
+    }
+
+    for (final n in ast.roots) {
+      if (walk(n)) return true;
+    }
+    return false;
   }
 }
---
+++ test/src/writor/dart/dart_generator_test.dart
@@ -0,0 +1,104 @@
+import 'dart:async';
+
+import 'package:locale_gen/src/model/locale_gen_params.dart';
+import 'package:locale_gen/src/writer/dart/dart_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('LocaleGenDartGenerator MessageFormat support', () {
+    final params = LocaleGenParams.fromYamlString('locale_gen', '''
+name: example
+locale_gen:
+  languages: ['en', 'nl']
+''');
+    final generator = LocaleGenDartGenerator();
+
+    String generate(Map<String, Map<String, dynamic>> all) {
+      return generator.createLocalizationFile(params, all['en']!, all);
+    }
+
+    test('emits _mf and message_format import when MessageFormat keys exist', () {
+      final out = generate({
+        'en': {'greeting': 'Hi, {name}!'},
+        'nl': {'greeting': 'Hallo, {name}!'},
+      });
+      expect(out, contains("import 'package:intl/intl.dart';"));
+      expect(out, contains("import 'package:intl/message_format.dart';"));
+      expect(out, contains('String _mf('));
+      expect(out, contains('LocalizedValue greeting({required String name})'));
+    });
+
+    test('does not emit Flutter imports', () {
+      final out = generate({
+        'en': {'greeting': 'Hi, {name}!'},
+        'nl': {'greeting': 'Hallo, {name}!'},
+      });
+      expect(out, isNot(contains('package:flutter/widgets.dart')));
+      expect(out, isNot(contains('package:flutter/services.dart')));
+    });
+
+    test('emits a _mf call per language inside LocalizedValue', () {
+      final out = generate({
+        'en': {'greeting': 'Hi, {name}!'},
+        'nl': {'greeting': 'Hallo, {name}!'},
+      });
+      expect(out, contains("en: _mf(r\"Hi, {name}!\", args: {'name': name}, locale: 'en')"));
+      expect(out, contains("nl: _mf(r\"Hallo, {name}!\", args: {'name': name}, locale: 'nl')"));
+    });
+
+    test('plural generates a num count param returning LocalizedValue', () {
+      final out = generate({
+        'en': {'cart_count': '{count, plural, one {# item} other {# items}}'},
+        'nl': {'cart_count': '{count, plural, one {# stuk} other {# stuks}}'},
+      });
+      expect(out, contains('LocalizedValue cartCount({required num count})'));
+      expect(out, contains("args: {'count': count}"));
+    });
+
+    test('camelCases uppercase names but preserves original key in args map', () {
+      final out = generate({
+        'en': {'confirm_terms': 'See {SC} please'},
+        'nl': {'confirm_terms': 'Zie {SC} aub'},
+      });
+      expect(out, contains('LocalizedValue confirmTerms({required String sc})'));
+      expect(out, contains("'SC': sc"));
+    });
+
+    test('emits _formatDuration helper when duration is used', () {
+      final out = generate({
+        'en': {'race': '{d, duration}'},
+        'nl': {'race': '{d, duration}'},
+      });
+      expect(out, contains('String _formatDuration(Duration d, String? style)'));
+      expect(out, contains('LocalizedValue race({required Duration d})'));
+      expect(out, contains('_formatDuration(d, null)'));
+    });
+
+    test('date short uses DateFormat.yMd with the locale baked in', () {
+      final out = generate({
+        'en': {'placedAt': 'Placed {placedAt, date, short}'},
+        'nl': {'placedAt': 'Geplaatst {placedAt, date, short}'},
+      });
+      expect(out, contains('LocalizedValue placedAt({required DateTime placedAt})'));
+      expect(out, contains("DateFormat.yMd('en').format(placedAt)"));
+      expect(out, contains("DateFormat.yMd('nl').format(placedAt)"));
+    });
+
+    test('cross-locale validator warns through Dart writer too', () {
+      final buf = StringBuffer();
+      runZoned(
+        () => generator.createLocalizationFile(params, {
+          'greeting': 'Hi, {name}!',
+        }, {
+          'en': {'greeting': 'Hi, {name}!'},
+          'nl': {'greeting': 'Hallo, {naam}!'},
+        }),
+        zoneSpecification: ZoneSpecification(
+          print: (_, __, ___, line) => buf.writeln(line),
+        ),
+      );
+      expect(buf.toString(), contains('greeting'));
+      expect(buf.toString(), contains('"nl"'));
+    });
+  });
+}
